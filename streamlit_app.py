e6c43ba593723aa10578c443512d411a723c5e4d
nvi-ey <nicolas.viguier@fr.ey.com>
2025-12-12 13:39:53 +0100

    Refactor smoothie order submission logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,24 +27,23 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    ingredients_string = ''
+	ingredients_string = ''
     
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+	for fruit_chosen in ingredients_list:
+		ingredients_string += fruit_chosen + ' '
+		st.subheader(fruit_chosen + ' nutrition information')
+		smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+		sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
     # st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+	my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
     #st.write(my_insert_stmt)
     #st.stop()
-    time_to_insert = st.button('Submit Order')
+	time_to_insert = st.button('Submit Order')
     
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+	if time_to_insert:
+		session.sql(my_insert_stmt).collect()
+		st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
